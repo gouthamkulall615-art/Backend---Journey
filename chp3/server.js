6733f31588209f3ea1e28ce2fc2aa85e0ddc9578
Goutham Kulall <gouthamkulall615@gmail.com>
2026-04-16 20:57:56 +0530

    understood and create a small user api and understood get and post requestusing hardcoded data

--- chp3/server.js
+++ chp3/server.js
@@ -1,14 +1,50 @@
 import http from "http";
-const PORT = 8000;
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+const PORT = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-    //   res.write("hello computer");
+console.log("SERVER FILE STARTED");
 
-   res.writeHead(500,{'content-type':'application/json'})
+//get current path
 
-  res.end(JSON.stringify({message:'server Erroe'}));
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
+
+const server = http.createServer(async (req, res) => {
+  //   res.write("hello computer");
+
+  // console.log(req.url);
+  // console.log(req.method)
+
+  try {
+    //check if a get request
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+         filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("not found");
+      }
+    } else {
+      throw new Error("method not found");
+    }
+    const data = await fs.readFile(filePath);
+
+    res.setHeader("content-type", "text/html");
+    res.write(data);
+    res.end();
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/html" });
+    res.end("<h1>server error</h1>");
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
