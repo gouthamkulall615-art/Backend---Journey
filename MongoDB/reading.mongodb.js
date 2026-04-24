6fef6f850cbe036152dcefdf007c729c2c74b88e
Goutham Kulall <gouthamkulall615@gmail.com>
2026-04-24 18:23:37 +0530

    understood the reading documents from mongodb

--- MongoDB/reading.mongodb.js
+++ MongoDB/reading.mongodb.js
@@ -1,2 +1,19 @@
 use("ecommerce");
-db.products.find({ name: "Wireless Mouse" });
+// db.products.find({ name: "Wireless Mouse" });
+// db.products.find({ category: "Electronics" });
+// db.products.find({ price: { $gt: 1000 } });
+
+//gt means greater than
+//gte means greater than or equal to
+//lt means less than
+//lte means less than or equal to
+
+// db.products.find({ price: { $gt: 1000, $lte: 50000 } }); --> and condtion
+
+// db.products.find({
+//   $or: [{ category: "Electronics" }, { stock: { $lt: 50 } }],
+// });--> OR condition
+
+// db.products.find({}, { name: 1, price: 1, _id: 0 })-->select the specific fields
+
+db.products.find().sort({ price: -1 }).skip(1).limit(2);
