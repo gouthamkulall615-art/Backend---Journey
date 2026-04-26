4f6cd939c5ca1f50d5059f7df017223a41209658
Goutham Kulall <gouthamkulall615@gmail.com>
2026-04-26 20:58:36 +0530

    registering user and monogdb database setup

--- Authentication/src/routes/auth.routes.js
+++ Authentication/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import * as authController from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
+//post /api/auth/register
 
+authRouter.post("/register", authController.register);
 
 export default authRouter;
