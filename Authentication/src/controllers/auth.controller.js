6fbc509a95ba2ea7aa2cb3da190a285c0d3bd28e
Goutham Kulall <gouthamkulall615@gmail.com>
2026-04-27 18:37:35 +0530

    identified user from request

--- Authentication/src/controllers/auth.controller.js
+++ Authentication/src/controllers/auth.controller.js
@@ -46,3 +46,22 @@ export async function register(req, res) {
     token,
   });
 }
+
+export async function getMe(req, res) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "token not found" });
+  }
+  const decoded = jwt.verify(token, config.JWT_SECRET);
+
+  const user = await userModel.findById(decoded.id);
+
+  res.status(200).json({
+    message: "user fetched successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
--- Authentication/src/routes/auth.routes.js
+++ Authentication/src/routes/auth.routes.js
@@ -7,4 +7,8 @@ const authRouter = Router();
 
 authRouter.post("/register", authController.register);
 
+
+//Get /api/auth/get-me
+authRouter.get("/get-me",authController.getMe)
+
 export default authRouter;
