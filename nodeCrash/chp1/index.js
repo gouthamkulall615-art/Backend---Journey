ec90b5d60d4bdf4e5be65b89f172a6b66aeab03c
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-18 18:25:14 +0530

    built an small routing system

--- nodeCrash/chp1/index.js
+++ nodeCrash/chp1/index.js
@@ -1,10 +1,23 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
+const url = require("url");
 
 //server
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview section");
+  } else if (pathName === "/product") {
+    res.end("this is the product section");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
