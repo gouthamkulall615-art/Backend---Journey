671e3cb595d548e544716a9b3e5f8cce8d398fe2
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-26 19:26:42 +0530

    buit and understood what is promises in js

---
+++ nodeCrash/asynchronousjs/promise.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const superagent = require("superagent");
+const { resolve, reject } = require("superagent/lib/request-base");
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) return reject("I could not find that file");
+
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) return reject("could not write file");
+      resolve("success");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog image saved to file!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
