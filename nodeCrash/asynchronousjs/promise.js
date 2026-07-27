c67ec6d0f1846eadff95721e07b186012e5ee88e
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-27 21:22:29 +0530

    done with the async and await

--- nodeCrash/asynchronousjs/promise.js
+++ nodeCrash/asynchronousjs/promise.js
@@ -21,20 +21,64 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed:${data}`);
 
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+
     console.log(res.body.message);
 
-    return writeFilePro("dog-img.txt", res.body.message);
-  })
-  .then(() => {
+    await writeFilePro("dog-img.txt", res.body.message);
+
     console.log("Random dog image saved to file!");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  } catch (err) {
+    console.log(err);
+
+    throw err;
+  }
+  return "2:READY 🐶";
+};
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics!");
+    const x = await getDogPic();
+    console.log(x)
+    console.log("3:Done getting dog pics");
+  } catch (err) {
+    console.log("Error");
+  }
+})();
+
+// console.log("1: Will get dog pics!");
+
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log("3:Done getting dog pics");
+//   })
+//   .catch((err) => {
+//     console.log("Error");
+//   });
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("Random dog image saved to file!");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
