c3e98f2ec57033f2e60e570bd61b04f7f6942d00
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-28 19:09:51 +0530

    handled the post api call

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -20,11 +21,33 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    results:tours.length,
+    results: tours.length,
     data: {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+
 });
 const PORT = 3000;
 
