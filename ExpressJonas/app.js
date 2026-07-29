5c15a6eb90dfd664a066dc2bcde78ca2aacd0d8d
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-29 08:54:16 +0530

    created a date middleware

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -4,10 +4,20 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
