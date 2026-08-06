9fa4c3ece45519e1abd7532eaefe52c4f22948b2
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-06 18:54:49 +0530

    done with the updating documents

--- ExpressJonas/controllers/tourController.js
+++ ExpressJonas/controllers/tourController.js
@@ -59,13 +59,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  //findByIdAndUpdate is an query
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
