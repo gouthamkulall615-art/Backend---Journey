876af27ffab02f1763059303034d5d4242d945bb
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-12 19:15:04 +0530

    done with the better refactoring of the error handlers

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //Middelwares
@@ -23,26 +24,14 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cant find ${req.originalUrl} on this server`,
-  // });
+  // const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  const err = new Error(`Cant find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Cant find ${req.originalUrl} on this server`), 404);
 });
 
 //Error handling middelware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
