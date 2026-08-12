9ff215a813fc301b177d3abdd31f06fb6710340f
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-12 19:03:07 +0530

    implemented the global error handling middleware

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -23,9 +23,25 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cant find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+//Error handling middelware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
