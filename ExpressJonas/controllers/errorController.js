6cc57b65659446ea713c96eed683cc7f57ebc73d
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-16 14:16:09 +0530

    handled the duplicates name field

--- ExpressJonas/controllers/errorController.js
+++ ExpressJonas/controllers/errorController.js
@@ -5,6 +5,15 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+
+  return new AppError(message, 400);
+};
+
 const sendErroDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -47,6 +56,7 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') {
       error = handleCastErrorDB(err);
     }
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
 
     sendErrorProd(error, res);
   }
