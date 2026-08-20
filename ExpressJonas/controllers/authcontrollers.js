a36f437ff4c65a39ad85b24d9a230232685712e9
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-20 09:06:04 +0530

    successfully sent the email to the nodemailer and reset token

--- ExpressJonas/controllers/authcontrollers.js
+++ ExpressJonas/controllers/authcontrollers.js
@@ -118,5 +118,26 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   //3. send it to users email
   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `fogot your password ? submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n if you didnt forget your password ,please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'your password reset token (valid till 10 minutes) ',
+      message,
+    });
+    res.status(200).json({
+      status: 'sucess',
+      message: 'Token sent to email',
+    });
+  } catch (err) {
+    user.createPasswordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError('There was an error sending the email, try again', 500),
+    );
+  }
 });
 exports.resetPassoword = (req, res, next) => {};
