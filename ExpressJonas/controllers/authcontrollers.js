1fef1ff0f1193b021e97a03d9a916757173cd181
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-20 09:25:22 +0530

    implemented the getting the user based on the token

--- ExpressJonas/controllers/authcontrollers.js
+++ ExpressJonas/controllers/authcontrollers.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -140,4 +142,33 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
-exports.resetPassoword = (req, res, next) => {};
+exports.resetPassoword = catchAsync(async (req, res, next) => {
+  //1.get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    createPasswordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  //2.if token has not expired and there is user set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.createPasswordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  //3 update changedPasswordAt property for the user
+
+  //4 log the user in , send jwt
+  res.status(200).json({
+    status: 'sucess',
+    token,
+  });
+});
