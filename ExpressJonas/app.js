2f27ed826280cc91b795fe2b8577e41172b14963
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-21 13:18:20 +0530

    understood the data sanitization

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -22,6 +24,11 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+//data sanitizatio against NoSQL query injection
+
+app.use(mongoSanitize());
+//data sanitization against xss
+app.use(xss());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
