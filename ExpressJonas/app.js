b7020817bfd18a69bc20c47440eebbb4954f8950
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-21 12:54:58 +0530

    implemented the rate limiting for request

--- ExpressJonas/app.js
+++ ExpressJonas/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -10,6 +11,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowsMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP,please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
