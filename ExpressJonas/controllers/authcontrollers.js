c7720fc2ad46686c41075f3371dac201d932739f
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-21 12:47:33 +0530

    stored the jwt in the cookies

--- ExpressJonas/controllers/authcontrollers.js
+++ ExpressJonas/controllers/authcontrollers.js
@@ -13,6 +13,19 @@ const signToken = (id) =>
   });
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+  //remove the password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -26,7 +39,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
     role: req.body.role,
   });
   createSendToken(newUser, 201, res);
@@ -185,7 +198,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
     return next(new AppError('your current password is wrong', 401));
   }
-  
+
   //3 if so update password
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
