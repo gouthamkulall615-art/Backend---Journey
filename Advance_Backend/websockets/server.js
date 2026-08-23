720919afbecba9d3893df74ae343e3c9ab10a417
Goutham Kulall <gouthamkulall615@gmail.com>
2026-08-23 15:16:58 +0530

    learned about the websockets

---
+++ Advance_Backend/websockets/server.js
@@ -0,0 +1,50 @@
+const WebSocket = require("ws");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT ?? 8956;
+
+// Create HTTP server
+const httpServer = http.createServer(async function (req, res) {
+  const indexFile = await fs.promises.readFile(
+    path.resolve("./index.html"),
+    "utf-8",
+  );
+
+  res.setHeader("Content-Type", "text/html");
+  res.end(indexFile);
+});
+
+// Create WebSocket server
+const wsServer = new WebSocket.Server({
+  server: httpServer,
+});
+
+// When a client connects
+wsServer.on("connection", (websocket) => {
+  console.log("WebSocket connection...");
+
+  // When client sends a message
+  websocket.on("message", (data) => {
+    console.log("WebSocket message received:", data.toString());
+
+    // Send response back to client
+    
+  });
+
+  // When connection closes
+  websocket.on("close", () => {
+    console.log("WebSocket connection closed");
+  });
+
+  // If an error occurs
+  websocket.on("error", (error) => {
+    console.log("WebSocket error:", error);
+  });
+});
+
+// Start HTTP + WebSocket server
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
